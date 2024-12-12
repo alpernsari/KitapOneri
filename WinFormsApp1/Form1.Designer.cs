e22775e3cec07070040bdfde2fb05e058b5adfbb
alpernsari <alperensari1453@gmail.com>
2024-12-12 14:36:56 +0300

    first commit

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label3 = new Label();
+            lbSecilenKitaplar = new ListBox();
+            lbKitapList = new ListBox();
+            btnSwitchRight = new Button();
+            btnSwitchLeft = new Button();
+            btnRastgeleOner = new Button();
+            lbOneriKitaplar = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(101, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Kitap Öneri Listesi";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(577, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(87, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Seçilen Kitaplar";
+            // 
+            // lbSecilenKitaplar
+            // 
+            lbSecilenKitaplar.FormattingEnabled = true;
+            lbSecilenKitaplar.ItemHeight = 15;
+            lbSecilenKitaplar.Location = new Point(577, 31);
+            lbSecilenKitaplar.Margin = new Padding(3, 2, 3, 2);
+            lbSecilenKitaplar.Name = "lbSecilenKitaplar";
+            lbSecilenKitaplar.Size = new Size(300, 394);
+            lbSecilenKitaplar.TabIndex = 6;
+            // 
+            // lbKitapList
+            // 
+            lbKitapList.FormattingEnabled = true;
+            lbKitapList.ItemHeight = 15;
+            lbKitapList.Location = new Point(12, 31);
+            lbKitapList.Margin = new Padding(3, 2, 3, 2);
+            lbKitapList.Name = "lbKitapList";
+            lbKitapList.Size = new Size(321, 394);
+            lbKitapList.TabIndex = 7;
+            // 
+            // btnSwitchRight
+            // 
+            btnSwitchRight.Location = new Point(382, 31);
+            btnSwitchRight.Name = "btnSwitchRight";
+            btnSwitchRight.Size = new Size(135, 85);
+            btnSwitchRight.TabIndex = 8;
+            btnSwitchRight.Text = "->";
+            btnSwitchRight.UseVisualStyleBackColor = true;
+            btnSwitchRight.Click += btnSwitchRight_Click;
+            // 
+            // btnSwitchLeft
+            // 
+            btnSwitchLeft.Location = new Point(382, 134);
+            btnSwitchLeft.Name = "btnSwitchLeft";
+            btnSwitchLeft.Size = new Size(135, 78);
+            btnSwitchLeft.TabIndex = 9;
+            btnSwitchLeft.Text = "<-";
+            btnSwitchLeft.UseVisualStyleBackColor = true;
+            btnSwitchLeft.Click += btnSwitchLeft_Click;
+            // 
+            // btnRastgeleOner
+            // 
+            btnRastgeleOner.Location = new Point(382, 291);
+            btnRastgeleOner.Name = "btnRastgeleOner";
+            btnRastgeleOner.Size = new Size(135, 90);
+            btnRastgeleOner.TabIndex = 10;
+            btnRastgeleOner.Text = "Kitap Öner";
+            btnRastgeleOner.UseVisualStyleBackColor = true;
+            btnRastgeleOner.Click += btnRastgeleOner_Click;
+            // 
+            // lbOneriKitaplar
+            // 
+            lbOneriKitaplar.FormattingEnabled = true;
+            lbOneriKitaplar.ItemHeight = 15;
+            lbOneriKitaplar.Location = new Point(12, 430);
+            lbOneriKitaplar.Name = "lbOneriKitaplar";
+            lbOneriKitaplar.Size = new Size(865, 409);
+            lbOneriKitaplar.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(903, 851);
+            Controls.Add(lbOneriKitaplar);
+            Controls.Add(btnRastgeleOner);
+            Controls.Add(btnSwitchLeft);
+            Controls.Add(btnSwitchRight);
+            Controls.Add(lbKitapList);
+            Controls.Add(lbSecilenKitaplar);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private Label label3;
+        private ListBox lbSecilenKitaplar;
+        private ListBox lbKitapList;
+        private Button btnSwitchRight;
+        private Button btnSwitchLeft;
+        private Button btnRastgeleOner;
+        private ListBox lbOneriKitaplar;
+    }
+}
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System.ComponentModel;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] satirlar = File.ReadAllLines("oneriler.txt");
+
+            foreach (string satir in satirlar)
+            {
+                // Satırı virgüllerden ayır
+                string[] parcalar = satir.Split(',');
+
+                if (parcalar.Length > 0)
+                {
+                    string name = parcalar[0] + "-" + parcalar[1] + "-" + parcalar[2];
+                    lbKitapList.Items.Add(name);
+                }
+            }
+        }
+
+        private void btnSwitchRight_Click(object sender, EventArgs e)
+        {
+            if (lbKitapList.SelectedItem != null)
+            {
+                lbSecilenKitaplar.Items.Add(lbKitapList.SelectedItem.ToString());
+                lbKitapList.Items.RemoveAt(lbKitapList.SelectedIndex);
+            }
+        }
+
+        private void btnSwitchLeft_Click(object sender, EventArgs e)
+        {
+            if (lbSecilenKitaplar.SelectedItem != null)
+            {
+                lbKitapList.Items.Add(lbSecilenKitaplar.SelectedItem.ToString());
+                lbSecilenKitaplar.Items.Remove(lbSecilenKitaplar.SelectedItem);
+            }
+        }
+
+        private void btnRastgeleOner_Click(object sender, EventArgs e)
+        {
+            lbOneriKitaplar.Items.Clear();
+
+            // Kategorilerin frekanslarını belirle
+            Dictionary<string, int> kategoriFrekans = new Dictionary<string, int>();
+
+            foreach (var item in lbSecilenKitaplar.Items)
+            {
+                string kitapBilgisi = item.ToString();
+                string kategori = kitapBilgisi.Split('-')[0].Trim();
+
+                if (kategoriFrekans.ContainsKey(kategori))
+                {
+                    kategoriFrekans[kategori]++;
+                }
+                else
+                {
+                    kategoriFrekans[kategori] = 1;
+                }
+            }
+
+            // Tüm kitapları kitaplar.txt'den oku ve kategorilere göre grupla
+            Dictionary<string, List<string>> kategoriBazliKitaplar = new Dictionary<string, List<string>>();
+            string[] satirlar = File.ReadAllLines("kitaplar.txt");
+
+            foreach (string satir in satirlar)
+            {
+                string[] parcalar = satir.Split(',');
+
+                if (parcalar.Length > 0)
+                {
+                    string kategori = parcalar[0].Trim();
+
+                    if (!kategoriBazliKitaplar.ContainsKey(kategori))
+                    {
+                        kategoriBazliKitaplar[kategori] = new List<string>();
+                    }
+
+                    kategoriBazliKitaplar[kategori].Add(satir);
+                }
+            }
+
+            // Rastgele kitapları ağırlıklı olarak seç
+            Random rastgele = new Random();
+            List<string> rastgeleSecilenKitaplar = new List<string>();
+
+            int toplamFrekans = kategoriFrekans.Values.Sum();
+            foreach (var kategori in kategoriFrekans)
+            {
+                if (kategoriBazliKitaplar.ContainsKey(kategori.Key))
+                {
+                    int kategoriyeAitKitapSayisi = (kategori.Value * 40) / toplamFrekans; // Ağırlıklı kitap sayısı
+                    List<string> mevcutKategoriKitaplar = kategoriBazliKitaplar[kategori.Key]
+                        .OrderBy(x => rastgele.Next())
+                        .Take(kategoriyeAitKitapSayisi)
+                        .ToList();
+
+                    rastgeleSecilenKitaplar.AddRange(mevcutKategoriKitaplar);
+                }
+            }
+
+            // Geriye kalan kitapları tamamla (Eğer 20'den az kaldıysa)
+            int eksikSayisi = 40 - rastgeleSecilenKitaplar.Count;
+            if (eksikSayisi > 0)
+            {
+                List<string> tumUygunKitaplar = kategoriBazliKitaplar
+                    .SelectMany(x => x.Value)
+                    .Where(x => !rastgeleSecilenKitaplar.Contains(x))
+                    .OrderBy(x => rastgele.Next())
+                    .Take(eksikSayisi)
+                    .ToList();
+
+                rastgeleSecilenKitaplar.AddRange(tumUygunKitaplar);
+            }
+
+            // Önerilen kitapları ListBox'a ekle
+            foreach (var item in rastgeleSecilenKitaplar)
+            {
+                lbOneriKitaplar.Items.Add(item);
+            }
+        }
+
+
+    }
+}
